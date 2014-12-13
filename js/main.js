9f795f80d6162dbce32d120c23a9a22ec4ea72fa
Nathan Kot <me@nathankot.com>
2014-12-13 14:22:21 +0900

    Support for toggling menu item active state.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/* jshint latedef:false */
 
+$(bootstrap);
+
+function bootstrap() {
+
+  'use strict';
+
+  /**
+   * For toggling active menu items.
+   */
+  $(window).on('hashchange', function() {
+    var hash = window.location.hash;
+    $('[href^="#"').closest('[toggle-active]').removeClass('active');
+    $('[href="' + hash + '"]').closest('[toggle-active]').addClass('active');
+  });
+
+}
