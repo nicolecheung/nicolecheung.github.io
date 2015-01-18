4a6b005565489c229f926e773bfafa4a7a61e4c1
Nathan Kot <me@nathankot.com>
2015-01-18 12:25:13 +0900

    Use angular to set up scrolling.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 /* jshint latedef:false */
 
-$(bootstrap);
+angular
+  .module('desk', [
+    'duScroll'
+  ]);
 
-function bootstrap() {
-
-  'use strict';
-
-  /**
-   * For toggling active menu items.
-   */
-  $(window).on('hashchange', function() {
-    var hash = window.location.hash;
-    $('[href^="#"').closest('[toggle-active]').removeClass('active');
-    $('[href="' + hash + '"]').closest('[toggle-active]').addClass('active');
-  });
-
-}
